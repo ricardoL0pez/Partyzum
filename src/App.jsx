0252c4d177952a94a61e089237a2670316443bce
tiago1820 <tiago.zdo@gmail.com>
2024-03-18 16:34:20 -0300

    Fue creado el component Chat. Fueron definidas la rutas iniciales del proyecto.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import { Chat } from './components/Chat/Chat';
+import './App.css'
+import Landing from './Views/Landing/Landing'
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Landing/>} />
+        <Route path='/chat' element={<Chat/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Views/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import fire from '../../assets/chubby-cute.gif'
+
+const Landing = () => {
+  return (
+    <>
+      <img src={fire} style={{ width: '150px' }} alt="" />
+      <h2>Arrancamos</h2>
+    </>
+  );
+};
+
+export default Landing;
