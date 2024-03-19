ed28c30848817e3d60df9c1408eb34af298d865e
tiago1820 <tiago.zdo@gmail.com>
2024-03-19 00:42:54 -0300

    Fue creada la estructura basica del chat con el component HomePage. Esta en construccion el componente Profile del chat.

---
+++ src/components/Chat/components/Profile/Profile.jsx
@@ -0,0 +1,13 @@
+import { BsArrowLeft, BsCheck2 } from 'react-icons/bs';
+export const Profile = ({ handleCloseOpenProfile }) => {
+    return (
+        <div className="w-full h-full">
+            <div className="flex items-center space-x-10 bg-[#008069] text-white pt-16 px-10 pb-5">
+                <div className='flex items-center space-x-10 bg-[#008069] text-white pt-16 px-10 pb-5'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
